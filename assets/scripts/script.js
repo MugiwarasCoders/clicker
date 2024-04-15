6211080b2635b5c8059d5e6d464f7554b3217719
burton-tyll <160846164+burton-tyll@users.noreply.github.com>
2024-04-15 16:16:27 +0200

    ajout de console log lorsqu'un seuil est atteint

---
+++ assets/scripts/script.js
@@ -0,0 +1,12 @@
+let pixelitePerSecond = 0;
+let clickerGain = 1;
+let counter = 0;
+
+//Prix
+
+let DigiBot_Price = 20;
+let GigaBot_Price = 30; 
+
+$(document).ready(function(){
+    $('.mine').click(clickAddPixelite)
+})
